76e4b66fd739ddaa828c9392498efeeda9c0d2af
snerks <martin.github@outlook.com>
2020-04-16 17:01:58 +0100

    Tidy code

--- index.ts
+++ index.ts
@@ -2,12 +2,13 @@ import { spawn } from "child_process";
 
 console.log("Hello from Typescript!!!");
 
-// const path = "C:\\\\D\\SamplesReact\\mui-theme-switcher-01";
+const path = "C:\\D\\SamplesReact\\mui-theme-switcher-01";
 // const path = "C:\\\\D\\SamplesReact\\football-stats-03";
 // const path = "BOGUS";
+
 const shell = "Git Bash";
 
-const gitBashPath = `"C:\\\\Program Files\\Git\\git-bash.exe"`;
+const gitBashPath = `"C:\\Program Files\\Git\\git-bash.exe"`;
 console.log(`launching ${shell} at shell path: ${gitBashPath}`);
 console.log(`launching ${shell} with folder path: ${path}`);
 
